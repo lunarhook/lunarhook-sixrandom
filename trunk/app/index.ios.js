82f0670331be84af2300a6d02e1ea4980bbb5525
fv008 <fv008@c6432ab3-e322-4368-8cea-da4f370a2f1f>
2017-07-11 19:08:27 +0000

    六爻项目提交

--- trunk/app/index.ios.js
+++ trunk/app/index.ios.js
@@ -4,30 +4,16 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry,Navigator,StyleSheet,View,  Text} from 'react-native';
+import MainPage from '../app/src/MainPage';
+
 
 export default class app extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+            <MainPage />
+        )
   }
 }
 
---
+++ trunk/app/src/MainPage.js
@@ -0,0 +1,42 @@
+
+var Dimensions = require('Dimensions');
+import React, {Component} from 'react';
+import {StyleSheet,View,  Text,WebView} from 'react-native';
+
+
+var kWidth = Dimensions.get('window').width;
+var kHeight = Dimensions.get('window').height;
+var WEBVIEW_REF = 'webview';
+var DEFAULT_URL = 'https://weibo.com/vczero';
+
+var MainPage = React.createClass({
+  render(){
+    return (
+      <View style={styles.container}>
+        <WebView
+          ref={WEBVIEW_REF}
+          automaticallyAdjustContentInsets={false}
+          style={styles.webView}
+          source={{uri: DEFAULT_URL}}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          decelerationRate="normal"
+          onNavigationStateChange={this.onNavigationStateChange}
+          onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+          startInLoadingState={true}
+        />
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create ({
+  container: {
+    flex:1
+  },
+  webSize: {
+    width:kWidth,
+    height:kHeight
+  }
+});
+module.exports=MainPage;  
