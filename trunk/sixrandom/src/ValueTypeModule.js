76e47db7ea65a1f2332ff3cf45d5519757cdbda2
fv008 <fv008@c6432ab3-e322-4368-8cea-da4f370a2f1f>
2017-08-04 20:26:38 +0000

    六爻项目提交 8月3号版本

---
+++ trunk/sixrandom/src/ValueTypeModule.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react';
+
+var 	ValueTypeModule =  {
+			
+	"emotion": "感情",
+	"bussiness": "事业",
+	"lucky": "运气",
+	"sued": "官司",
+"health":"健康",
+"finance":"求财",
+}
+
+module.exports=ValueTypeModule;  
