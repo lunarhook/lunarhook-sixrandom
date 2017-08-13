e1a638874c0c0851be9cd1edb09600d8c4eceb01
fv008 <fv008@c6432ab3-e322-4368-8cea-da4f370a2f1f>
2017-08-13 20:51:26 +0000

    六爻项目提交 v1.2

--- trunk/sixrandom/iosbundle.sh
+++ trunk/sixrandom/iosbundle.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+cd /Applications/svn/sixrandom/sixrandom/
 react-native bundle --entry-file ./index.ios.js --bundle-output ./ios/bundle/index.ios.jsbundle --platform ios --assets-dest ./ios/bundle --dev false
