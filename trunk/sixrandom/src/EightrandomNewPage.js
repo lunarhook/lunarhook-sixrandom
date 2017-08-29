f3232e59ed1e8805de946bafd1979f59e308238e
fv008 <fv008@c6432ab3-e322-4368-8cea-da4f370a2f1f>
2017-08-29 06:50:38 +0000

    六爻项目提交 v1.4 增加日历功能

--- trunk/sixrandom/src/EightrandomNewPage.js
+++ trunk/sixrandom/src/EightrandomNewPage.js
@@ -12,9 +12,7 @@ import SixrandomModule from './SixrandomModule'
 import DateTimePicker from 'react-native-modal-datetime-picker';
 import { RadioButtons } from 'react-native-radio-buttons'
 import {RadioGroup, RadioButton} from 'react-native-flexi-radio-button'
-const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-var dataArray = [];
-var randArray = []
+
 
 class EightrandomNewPage extends React.Component {
 
@@ -24,11 +22,10 @@ class EightrandomNewPage extends React.Component {
         this.state= {
           isDatePickerVisible: false,
           isTimePickerVisible: false,
-           dataSource: ds.cloneWithRows(dataArray),
+      
             selectedValue: '女',
             datepicker:"",
             timepicker:"",
-            Step: 7,
             Tip: ""
     }
    
@@ -139,79 +136,24 @@ class EightrandomNewPage extends React.Component {
         </RadioGroup>
         
             </View>
+            <Button
+  onPress={()=>this.bazipaipan()}
+  title="八字排盘"
+  //accessibilityLabel="Learn more about this purple button"
+/>
             </View> 
             )
     }
-  random()
-  {
-    //alert(dataArray)
-    if(this.state.Step>0)
-    {    
-      var t=t0=t1=t2=0
-      t0= Math.random(1)>=0.5?1:0 
-      t1= Math.random(1)>=0.5?1:0 
-      t2= Math.random(1)>=0.5?1:0 
-      t= t0+t1+t2+6
-       //alert(t)
-     
-      var x = ""
-      if(t==8)
-      {
-        x = SixrandomModule.getnegativedraw()
-      }
-      else if(t==6)
-      {
-        x = SixrandomModule.getnegativedraw()
-      }
-      else if(t==7)
-      {
-        x = SixrandomModule.getpositivedraw()
-      }
-      else if(t==9)
-      {
-        x = SixrandomModule.getpositivedraw()
-      }
-      randArray[(this.state.Step-1)] = t
-      dataArray[(this.state.Step-1)] = x
-      this.setState({Step: this.state.Step-1})
-      this.setState({dataSource: ds.cloneWithRows(dataArray)})
-    }
-    if(this.state.Step==1)
+    bazipaipan()
     {
-      randArray[0] = this.state.selectedValue;
-      var index = (new Date()).valueOf().toString();
-      randArray[7] = index;
-      randArray[8] = this.state.Tip
-      StorageModule.save({key:"last",data:randArray})
-      StorageModule.save({key:"user",id:index,data:randArray})
-      this.props.navigation.state.params = randArray
-      //alert(this.props.navigation.state.params)
-      var date = new Date(Number(randArray[7]))
-      var lunar = ""
-      for (i =1;i<7;i++)
-      {
-        lunar = lunar+(randArray[i]).toString()
-      }
-      var question = randArray[0]
-      var parameter = "?date="+(new Date(Number(randArray[7])))+"&lunar="+lunar+"&question="+question
-      this.props.navigation.navigate('SixrandomMainPage',parameter)
-      //this.begin('SixrandomMainPage')
-      this.picker("emotion")
-     
+      var dataArray = [];
+      dataArray["date"] = this.state.datepicker;
+      dataArray["time"] = this.state.timepicker;
+      dataArray["sex"]  = this.state.selectedValue;
+      dataArray["name"] = this.state.Tip
+
+      console.log(dataArray)
     }
-          
-     
-    //this.flatlist.extraData=[]
-  }
-  picker(value)
-  {
-    dataArray = []
-    randArray = []
-    this.setState({selectedValue: value})
-    this.setState({Step: 7})
-    this.setState({dataSource: ds.cloneWithRows(dataArray)})
-   // this.flatlist.refresh()
-  }
   begin(pagename)
     {
       const resetAction = NavigationActions.reset({
