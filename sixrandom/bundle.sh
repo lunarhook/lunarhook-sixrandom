3443df12c500ff868e47411d97a5ef59badc37f7
fv0008 <developer@lunarhook.com>
2020-04-28 21:07:27 +0800

    bundle to 6

--- sixrandom/bundle.sh
+++ sixrandom/bundle.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 basepath=$(cd `dirname $0`; pwd)
-node node_modules/react-native/local-cli/cli.js bundle --entry-file ./index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --platform android --assets-dest ./android/app/src/main/res/ --dev false --max-workers 2 --verbose
-node node_modules/react-native/local-cli/cli.js bundle --entry-file ./index.js --bundle-output ./ios/main.jsbundle --platform ios --assets-dest ./ios/bundle --dev false --max-workers 2 --verbose
+node node_modules/react-native/local-cli/cli.js bundle --entry-file ./index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --platform android --assets-dest ./android/app/src/main/res/ --dev false --max-workers 6 --verbose
+node node_modules/react-native/local-cli/cli.js bundle --entry-file ./index.js --bundle-output ./ios/main.jsbundle --platform ios --assets-dest ./ios/bundle --dev false --max-workers 6 --verbose
