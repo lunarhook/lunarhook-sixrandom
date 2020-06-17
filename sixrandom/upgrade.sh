9b2d5dadd6c0bf5fd276e25e883e9859141692b2
fanxiaolong <developer@lunarhook.com>
2020-06-18 02:42:10 +0800

    Update upgrade.sh

--- sixrandom/upgrade.sh
+++ sixrandom/upgrade.sh
@@ -17,7 +17,9 @@ npm config set registry https://registry.npm.taobao.org --global
 npm config set disturl https://npm.taobao.org/dist --global
 yarn global add react-native-cli
 npm install -g npm-check-updates
+git stash
 git pull
+git stash pop
 ncu -u -x react-native
 yarn global add @tarojs/cli@latest
 yarn config set registry https://registry.npm.taobao.org --global
