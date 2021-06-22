95e5549089c16f4e5dce426f8ff7af6b1ce2d56c
fanxiaolong <developer@lunarhook.com>
2021-06-23 02:14:59 +0800

    1

--- sixrandom/ChangeBrew.sh
+++ sixrandom/ChangeBrew.sh
@@ -7,6 +7,19 @@ git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna
 git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git
 git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-drivers.git
 
+
+# 或使用下面的几行命令自动设置
+BREW_TAPS="$(brew tap)"
+for tap in core cask{,-fonts,-drivers,-versions}; do
+    if echo "$BREW_TAPS" | grep -qE "^homebrew/${tap}\$"; then
+        # 将已有 tap 的上游设置为本镜像并设置 auto update
+        # 注：原 auto update 只针对托管在 GitHub 上的上游有效
+        git -C "$(brew --repo homebrew/${tap})" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
+        git -C "$(brew --repo homebrew/${tap})" config homebrew.forceautoupdate true
+    else   # 在 tap 缺失时自动安装（如不需要请删除此行和下面一行）
+        brew tap --force-auto-update homebrew/${tap} https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-${tap}.git
+    fi
+done
 # 以下针对 Linux 系统上的 Linuxbrew
-git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/linuxbrew-core.git
-brew update
+#git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/linuxbrew-core.git
+brew update-reset
--- sixrandom/RestoreBrew.sh
+++ sixrandom/RestoreBrew.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
-git -C "$(brew --repo)" remote set-url origin https://github.com/Homebrew/brew.git
+#git -C "$(brew --repo)" remote set-url origin https://github.com/Homebrew/brew.git
 
 # 以下针对 mac OS 系统上的 Homebrew
-git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/homebrew-core.git
-git -C "$(brew --repo homebrew/cask)" remote set-url origin https://github.com/Homebrew/homebrew-cask.git
-git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://github.com/Homebrew/homebrew-cask-fonts.git
-git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://github.com/Homebrew/homebrew-cask-drivers.git
+#git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/homebrew-core.git
+#git -C "$(brew --repo homebrew/cask)" remote set-url origin https://github.com/Homebrew/homebrew-cask.git
+#git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://github.com/Homebrew/homebrew-cask-fonts.git
+#git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://github.com/Homebrew/homebrew-cask-drivers.git
 
 # 以下针对 Linux 系统上的 Linuxbrew
-git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/linuxbrew-core.git
+#git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/linuxbrew-core.git
 
 # 更换后测试工作是否正常
-brew update
+#brew update
+
+# brew 程序本身，Homebrew / Linuxbrew 相同
+git -C "$(brew --repo)" remote set-url origin https://github.com/Homebrew/brew.git
+
+# 以下针对 macOS 系统上的 Homebrew
+BREW_TAPS="$(brew tap)"
+for tap in core cask{,-fonts,-drivers,-versions}; do
+    if echo "$BREW_TAPS" | grep -qE "^homebrew/${tap}\$"; then
+        git -C "$(brew --repo homebrew/${tap})" remote set-url origin https://github.com/Homebrew/homebrew-${tap}.git
+    fi
+done
+
+# 以下针对 Linux 系统上的 Linuxbrew
+# git -C "$(brew --repo homebrew/core)" remote set-url origin https://github.com/Homebrew/linuxbrew-core.git
+
+# 重新设置 git 仓库 HEAD
+brew update-reset
