64db80eb15b6e890d3e91a9165050592d3c07493
fanxiaolong <developer@lunarhook.com>
2022-02-03 16:24:42 +0800

    Update AppDelegate.m

--- sixrandom/ios/sixrandom/AppDelegate.m
+++ sixrandom/ios/sixrandom/AppDelegate.m
@@ -76,7 +76,12 @@ NSString *const RCTJSNavigationScheme = @"react-js-navigation";
                                                    moduleName:@"sixrandom"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  //rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+      rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+      rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
