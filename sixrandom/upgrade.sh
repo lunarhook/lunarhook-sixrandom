617465d729e262c5a5f4c0986974b838e4f901b5
fanxiaolong <developer@lunarhook.com>
2023-02-01 18:42:09 +0800

    Update upgrade.sh

--- sixrandom/upgrade.sh
+++ sixrandom/upgrade.sh
@@ -10,7 +10,7 @@ brew install git
 brew install go
 brew install httpd
 brew install icu4c
-brew install kubernetes-cli
+#brew install kubernetes-cli
 brew install maven
 brew install node
 brew install nghttp2
@@ -25,17 +25,17 @@ brew install readline
 brew install sqlite
 brew install watchman
 brew install yarn
-brew install ideviceinstaller ios-webkit-debug-proxy
+#brew install ideviceinstaller ios-webkit-debug-proxy
 brew install --cask android-sdk
 #brew install --cask homebrew/cask-versions/adoptopenjdk8
 brew cleanup
 npm config set registry https://registry.npm.taobao.org --global
 npm config set disturl https://npm.taobao.org/dist --global
-yarn global add react-native-cli
+#yarn global add react-native-cli
 npm install -g npm-check-updates
 git pull
 ncu -u -x react-native
-yarn global add @tarojs/cli@latest
+#yarn global add @tarojs/cli@latest
 yarn config set registry https://registry.npm.taobao.org --global
 yarn config set disturl https://npm.taobao.org/dist --global
 yarn
